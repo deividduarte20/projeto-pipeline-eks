9a13d4b24c2a78a61121bbe640308ac0a56a3aa8
deividduarte20 <deividdua@gmail.com>
2024-02-14 12:30:10 -0300

    update in pipeline

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -8,25 +8,25 @@ module "eks" {
   region          = var.region
 }
 
-# resource "null_resource" "eks_cluster_config" {
-#   # count = var.create_kubeconfig ? 1 : 0
+resource "null_resource" "eks_cluster_config" {
+  # count = var.create_kubeconfig ? 1 : 0
 
-#   depends_on = [
-#     module.eks.eks,
-#     module.eks.eks_cluster_ready,
-#   ]
+  depends_on = [
+    module.eks.eks,
+    module.eks.eks_cluster_ready,
+  ]
 
 
-#   provisioner "local-exec" {
-#     command = "sleep 30" # Aguarde 30 segundos para dar tempo ao cluster EKS de ser totalmente provisionado
-#   }
+  provisioner "local-exec" {
+    command = "sleep 30" # Aguarde 30 segundos para dar tempo ao cluster EKS de ser totalmente provisionado
+  }
 
-#   provisioner "local-exec" {
-#     command = <<-EOT
-#       aws eks --region ${var.region} update-kubeconfig --name ${var.name-cluster}
-#     EOT
-#   }
-# }
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws eks --region ${var.region} update-kubeconfig --name ${var.name-cluster}
+    EOT
+  }
+}
 
 # resource "null_resource" "executa_script" {
 #   depends_on = [null_resource.eks_cluster_config]
--- terraform-eks/provider.tf
+++ terraform-eks/provider.tf
@@ -1,3 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+provider "null" {
+
+}
