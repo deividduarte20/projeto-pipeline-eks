a3a74b996976c953265e1d0465dc989e9db37862
deividduarte20 <deividdua@gmail.com>
2024-02-18 12:31:48 -0300

    up repo template

--- terraform-eks/backend.tf
+++ terraform-eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "statetf2024" # Insira nome do bucket
+    bucket         = "$buck" # Insira nome do bucket
     key            = "state/terraform.tfstate" # Insira o nome do arquivo para state
     region         = "us-east-1"
   }
